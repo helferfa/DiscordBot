4d1c977c4fe2172d4e00bbee845932fd47dc44a7
Fabian helfer <helferfa@gmail.com>
2018-03-05 01:21:54 +0100

    Added VoiceListener, to track Server Activity into a Server Log

--- src/main/java/listeners/voiceListener.java
+++ src/main/java/listeners/voiceListener.java
@@ -1,6 +1,9 @@
 package listeners;
 
+import net.dv8tion.jda.core.events.StatusChangeEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.core.events.user.UserOnlineStatusUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 /**
@@ -12,9 +15,24 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class voiceListener extends ListenerAdapter {
 
     public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
-        event.getGuild().getTextChannelsByName("voicelog", true).get(0).sendMessage(
+        event.getGuild().getTextChannelsByName("server-log", true).get(0).sendMessage(
                 "Member " + event.getVoiceState().getMember().getUser().getName() + " joined voice channel " + event.getChannelJoined().getName() + "."
         ).queue();
+    }
+
+    public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
+        event.getGuild().getTextChannelsByName("server-log", true).get(0).sendMessage(
+                "Member " + event.getVoiceState().getMember().getUser().getName() + " moved to voice channel " + event.getChannelJoined().getName() + ".").queue();
+
+    }
+
+    /*public void  onStatusChange(StatusChangeEvent e) {
+        e.getJDA().getGuilds().get(0).getTextChannelsByName("bot-spam", true).get(0).sendMessage("Status Change: " + e.getStatus().name() + "from: " + e.getOldStatus().name()).queue();
+    }*/
+
+    public void onUserOnlineStatusUpdate(UserOnlineStatusUpdateEvent event) {
+
+        event.getGuild().getTextChannelsByName("server-log", true).get(0).sendMessage("Status Change: " + "User: " + event.getMember().getUser().getName() + " is now " + event.getCurrentOnlineStatus()).queue();
     }
 
 }
--- src/main/java/util/STATIC.java
+++ src/main/java/util/STATIC.java
@@ -2,7 +2,7 @@ package util;
 
 public class STATIC {
 
-    public static final String VERSION = "0.62";
+    public static final String VERSION = "0.73";
 
     public static final String PREFIX = "!" ;
 
